adf81db45611fbe8d7b73859d8241f2d7b967a81
M3NGSZE <mengse.thun13@gmail.com>
2026-08-16 15:10:52 +0700

    test new file

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,4 +1,17 @@
 defmodule Example do
+  use Application
+
+  def start(_type, _args) do
+    # code
+    IO.puts(:"Hello world")
+
+    hello()
+
+    IO.puts(:"sum: #{add(2, 3)}")
+
+    Supervisor.start_link([], strategy: :one_for_one)
+  end
+
   @moduledoc """
   Documentation for `Example`.
   """
